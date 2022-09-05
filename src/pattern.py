33ac57217552e58d73daa885be700d1eb107f6d8
MEskerod <112616513+MEskerod@users.noreply.github.com>
2022-09-05 10:50:57 +0200

    Made pattern

--- src/pattern.py
+++ src/pattern.py
@@ -1,2 +1,14 @@
 
 # Print the pattern
+
+pattern = "*"
+
+for i in range(9):
+    if i==0:
+        print(pattern)
+    elif i<5:
+        pattern = pattern + " *"
+        print(pattern)
+    else:
+        pattern = pattern[:-2]
+        print(pattern)
