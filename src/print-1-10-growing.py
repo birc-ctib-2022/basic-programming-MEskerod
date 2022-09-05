caeefbdd8eaaffb00238868cf58f781660853875
MEskerod <112616513+MEskerod@users.noreply.github.com>
2022-09-05 10:43:21 +0200

    Made growing numbers

--- src/print-1-10-growing.py
+++ src/print-1-10-growing.py
@@ -1,2 +1,10 @@
 
 # Print the numbers described in the exercise
+
+numbers="1"
+for i in range(1,11):
+    if i==1:
+        print(numbers)
+    else:
+        numbers=numbers+" "+str(i)
+        print(numbers)
