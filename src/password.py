b5d5f13ab6755dd219c53f2bf53bf17e53d12b0e
MEskerod <112616513+MEskerod@users.noreply.github.com>
2022-09-05 12:28:42 +0200

    1. attemp at password validation

--- src/password.py
+++ src/password.py
@@ -1,3 +1,5 @@
+from curses.ascii import isupper
+from pickle import FALSE, TRUE
 import sys
 
 if len(sys.argv) != 2:
@@ -8,5 +10,27 @@ password = sys.argv[1]
 is_valid = False
 
 # Do all the requirement checks here.
+lower_case = FALSE
+upper_case = FALSE
+number = FALSE
+special = FALSE
+
+for c in password:
+    if c.islower():
+        lower_case = TRUE
+    elif c.isupper():
+        upper_case = TRUE
+    elif c.isnumeric():
+        number = TRUE
+    elif c in "$#@":
+        special = TRUE
+    else: 
+        continue
+    if lower_case == TRUE:
+        if upper_case == TRUE:
+            if number == TRUE:
+                if special == TRUE:
+                    is_valid = TRUE
+                    break   
 
 print(is_valid)
